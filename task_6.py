4ddf3c6c614bfe8c92abf8ac21f6a9f3218fa471
Iurii Salov <Yury.Salov@mvideo.ru>
2022-07-18 16:33:23 +0300

    task6

--- task_6.py
+++ task_6.py
@@ -8,3 +8,17 @@ print('Задача 6. Письмо')
 # которая подскажет сколько раз нужно сложить письмо пополам,
 # чтобы оно поместилось в конверт.
 # Размеры письма вводятся с клавиатуры.
+
+packed_size = 12
+
+letter_size = int(input('input letter size: '))
+
+if letter_size > packed_size:
+    count = letter_size / (2 * packed_size)
+else:
+    count = 0
+
+if count % 1 > 0:
+    count = (count // 1 + 1)
+
+print('result:', count * 2)
